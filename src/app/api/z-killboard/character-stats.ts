16b6dc94cbcf2c29d6e986a4fa2f0a059b38b39e
Edgar Toll <edjopato@gmail.com>
2016-08-17 22:31:46 +0200

    0 if undefined

--- src/app/api/z-killboard/character-stats.ts
+++ src/app/api/z-killboard/character-stats.ts
@@ -31,20 +31,20 @@ export class CharacterStats {
   constructor(json: any) {
     this.type = json.type;
     this.id = json.id;
-    this.shipsLost = json.shipsLost;
-    this.pointsLost = json.pointsLost;
-    this.iskLost = json.iskLost;
-    this.shipsDestroyed = json.shipsDestroyed;
-    this.pointsDestroyed = json.pointsDestroyed;
-    this.iskDestroyed = json.iskDestroyed;
+    this.shipsLost = json.shipsLost || 0;
+    this.pointsLost = json.pointsLost || 0;
+    this.iskLost = json.iskLost || 0;
+    this.shipsDestroyed = json.shipsDestroyed || 0;
+    this.pointsDestroyed = json.pointsDestroyed || 0;
+    this.iskDestroyed = json.iskDestroyed || 0;
     // TODO: groups
     // TODO: months
     this.sequence = json.sequence;
 
     this.info = {
-      allianceID: json.info.allianceID,
-      corporationID: json.info.corporationID,
-      factionID: json.info.factionID,
+      allianceID: json.info.allianceID || 0,
+      corporationID: json.info.corporationID || 0,
+      factionID: json.info.factionID || 0,
       id: json.info.id,
       killID: json.info.killID,
       name: json.info.name,
