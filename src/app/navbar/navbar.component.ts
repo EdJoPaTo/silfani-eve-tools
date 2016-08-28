e5561a3ca294f9829a0b091c82d73a699edb0cad
Edgar Toll <edjopato@gmail.com>
2016-08-28 20:30:58 +0200

    use of bulma nav stuff

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -7,6 +7,7 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
   @Input() title: string;
+  navopen: boolean;
 
   constructor() { }
 
