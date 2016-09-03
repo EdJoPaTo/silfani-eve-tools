e66cd2cd9b0201fe6b708a07e42f64adc0c12c92
Edgar Toll <edjopato@gmail.com>
2016-09-03 17:04:58 +0200

    changed from namefilter rxjs/Subject to a simple and instant pipe

--- src/app/missions/missions.component.ts
+++ src/app/missions/missions.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { Subject } from 'rxjs/Subject';
 
 import { MissionService } from '../api/static-resources/mission.service';
 import { MissionListComponent } from './mission-list';
 
+import { NameFilterPipe } from './name-filter.pipe';
 import { DetailedinfoPipe } from './detailedinfo.pipe';
 import { InfoiconPipe } from './infoicon.pipe';
 import { InfotitlePipe } from './infotitle.pipe';
@@ -15,13 +15,17 @@ import { InfotitlePipe } from './infotitle.pipe';
   directives: [
     MissionListComponent
   ],
-  pipes: [DetailedinfoPipe, InfoiconPipe, InfotitlePipe],
+  pipes: [
+    NameFilterPipe,
+    DetailedinfoPipe,
+    InfoiconPipe,
+     InfotitlePipe
+   ],
   providers: [MissionService]
 })
 export class MissionsComponent implements OnInit {
   private missions = [];
-  private filterTerms = new Subject<string>();
-  private filtered = [];
+  namefilter = '';
 
   constructor(
     private missionService: MissionService
@@ -31,34 +35,6 @@ export class MissionsComponent implements OnInit {
     this.missionService.get()
       .subscribe(json => {
         this.missions = json;
-        this.filter('');
       });
-
-    this.filterTerms
-      .debounceTime(300)        // wait for 300ms pause in events
-      .distinctUntilChanged()   // ignore if next search term is same as previous
-      .map(term => this.getMissionsBasedOnTerm(term))
-      .subscribe(result => this.filtered = result);
-  }
-
-  filter(term: string) {
-    if (this.missions.length === 0) {
-      return;
-    }
-    this.filterTerms.next(term);
-  }
-
-  getMissionsBasedOnTerm(term: string): any[] {
-    term = term.toLowerCase();
-    if (!this.missions) { return []; }
-
-    return this.missions.filter(mission => {
-      if (mission.name.en.toLowerCase().indexOf(term) >= 0) {
-        return true;
-      }
-      if (mission.name.de.toLowerCase().indexOf(term) >= 0) {
-        return true;
-      }
-    });
   }
 }
