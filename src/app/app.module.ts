dfb3eba043dab8df034727b6f026efec5dc30445
Edgar Toll <edjopato@gmail.com>
2016-09-04 11:54:04 +0200

    use folder import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,15 +10,15 @@ import { IskPipe } from './isk.pipe';
 import { ShortnumberPipe } from './shortnumber.pipe';
 import { VolumePipe } from './volume.pipe';
 
-import { AboutComponent } from './about/about.component';
-import { ImpressumComponent } from './impressum/impressum.component';
-import { LegalComponent } from './legal/legal.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { AboutComponent } from './about';
+import { ImpressumComponent } from './impressum';
+import { LegalComponent } from './legal';
+import { NavbarComponent } from './navbar';
+import { PageNotFoundComponent } from './page-not-found';
 
 import { PilotAnalyzerComponent } from './pilot-analyzer';
-import { OverviewComponent } from './overview/overview.component';
-import { MissionsComponent } from './missions/missions.component';
+import { OverviewComponent } from './overview';
+import { MissionsComponent } from './missions';
 
 @NgModule({
   declarations: [
