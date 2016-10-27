f0ed18fb12671179dc94fb29b5990de52cbc40c8
Edgar Toll <edjopato@gmail.com>
2016-10-27 11:33:46 +0200

    added functionality to show shortnumber of integers; show ship destroyed/ lost as integer

--- src/app/shared/number-functions.ts
+++ src/app/shared/number-functions.ts
@@ -1,6 +1,6 @@
 const allLetters = ['', 'k', 'M', 'B', 'T'];
 
-export function formatNumberShort(value: number): string {
+export function formatNumberShort(value: number, isInteger = false): string {
   if (!value && value !== 0) { return 'NaN'; }
 
   let exponent = value !== 0 ? Math.ceil(Math.log10(value)) : 0;
@@ -10,6 +10,9 @@ export function formatNumberShort(value: number): string {
   let shortValue = value / Math.pow(10, engineerExponent);
 
   let fractionDigits = Math.min(2, 3 - (exponent - engineerExponent));
+  if (isInteger && engineerExponentLevel === 0) {
+    fractionDigits = 0;
+  }
 
   let valueString = shortValue.toFixed(fractionDigits);
   return valueString + letter;
