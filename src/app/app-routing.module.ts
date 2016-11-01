b220179914be666b8320385c58b5c2fc3e4df8cb
Edgar Toll <edjopato@gmail.com>
2016-11-01 11:18:40 +0100

    only routed components dont need an index file

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { PilotAnalyzerComponent } from './pilot-analyzer';
-
 import { AboutComponent, ImpressumComponent, LegalComponent, PageNotFoundComponent } from './core';
 import { OverviewComponent } from './overview';
 
