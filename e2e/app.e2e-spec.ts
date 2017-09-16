1319a70e9ad268737db3c330a798bdd7d007b14b
Edgar Toll <edjopato@gmail.com>
2017-09-16 18:23:44 +0200

    chore: update to @angular/cli 1.4.1

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,14 +1,14 @@
-import { SilfaniEveToolsPage } from './app.po';
+import { AppPage } from './app.po';
 
 describe('silfani-eve-tools App', () => {
-  let page: SilfaniEveToolsPage;
+  let page: AppPage;
 
   beforeEach(() => {
-    page = new SilfaniEveToolsPage();
+    page = new AppPage();
   });
 
-  it('should display message saying app works', () => {
+  it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('app works!');
+    expect(page.getParagraphText()).toEqual('Welcome to app!');
   });
 });
